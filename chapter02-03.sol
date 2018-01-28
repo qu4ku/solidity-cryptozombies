88a1cdc6cf7f83db024eb72a10fd9f27979c2760
qu4ku <qu4ku@hotmail.com>
2018-01-28 09:01:37 +0100

    solidity

--- chapter02-03.sol
+++ chapter02-03.sol
@@ -19,8 +19,10 @@ contract ZombieFactory {
 
     function _createZombie(string _name, uint _dna) private {
         uint id = zombies.push(Zombie(_name, _dna)) - 1;
+        zombieToOwner[id] = msg.sender;
+        ownerZombieCount[msg.sender]++; 
         NewZombie(id, _name, _dna);
-    } 
+    }
 
     function _generateRandomDna(string _str) private view returns (uint) {
         uint rand = uint(keccak256(_str));
